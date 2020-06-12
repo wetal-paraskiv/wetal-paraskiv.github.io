51433ed480947903632e2368265bbb1aa42b60b4
wetal-paraskiv <63596721+wetal-paraskiv@users.noreply.github.com>
2020-06-12 15:22:16 +0300

    Add files via upload

---
+++ myFirstScript.js
@@ -0,0 +1,15 @@
+function displayResult() {
+  document.getElementById("myP").innerHTML = "Wetali You REALLY Will like JAVASCRIPT!";
+  document.getElementById("divpoem").style.fontSize = "13px";
+  document.getElementById("divpoem").style.color = "white";
+}
+function bigText() {
+  document.getElementById("divpoem").style.fontSize = "25px";
+  document.getElementById("divpoem").style.color = "yellow";
+}
+function bigPicture() {
+  document.getElementById("Romania").style.width = "500px";
+}
+function smallPicture() {
+  document.getElementById("Romania").style.width = "250px";
+}
